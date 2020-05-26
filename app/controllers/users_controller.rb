247cdecebed076b9116bb737c023db3829a472dc
thibault mambour <thibault.mambour@protonmail.com>
2020-05-26 16:19:36 +0200

    finished controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,53 @@
 class UsersController < ApplicationController
 
+    def index
+        if params[:company_id] != nil
+            @users = User.where(:company_id => params[:company_id])
+            @company = Company.find(params[:company_id])
+        end
+    end
+
     def show
-        @user = User.find(current_user.id)
+        if params[:company_id] != nil
+            @user = User.find(params[:id])
+        else
+            @user = User.find(current_user.id)
+        end
     end
 
     def edit
+        if params[:format] == "Approved"
+            @approving = true
+        elsif params[:format] == "Join"
+            @join = true
+        end
+    
         @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(current_user.id)
-        @user.update(user_params)
-        redirect_to user_path(current_user.id)
+        if params[:user][:status] == "Approved"
+            @user = User.find(params[:id])
+            @user.status = params[:user][:status]
+            @user.save
+            redirect_to company_users_path(current_user.company.id)
+        elsif params[:user][:status] == "Remove access"
+            @user = User.find(params[:id])
+            @user.company = nil
+            @user.status = nil
+            @user.save
+            redirect_to company_users_path(current_user.company.id)
+        else
+            @user = User.find(current_user.id)
+            if @user.company == nil
+                @company = Company.where(:name => params[:user][:company_id])[0]
+                @user.company = @company
+                @user.status = "Waiting for approval"
+            end
+            @user.update(user_params)
+            redirect_to user_path(current_user.id)
+        end
+
     end
 
     def destroy
