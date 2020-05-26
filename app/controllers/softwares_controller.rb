01f0a877b1225722d72e4fc8c7dfdc5a31702916
vanessa massiot <vanessa.massiot@free.fr>
2020-05-26 08:47:18 +0200

    softwarescontroller basics methods

--- app/controllers/softwares_controller.rb
+++ app/controllers/softwares_controller.rb
@@ -1,2 +1,40 @@
 class SoftwaresController < ApplicationController
+
+	before_action :set_software, only: [ :show, :edit, :update, :destroy]
+	
+	def index
+	  @softwares = Software.all
+	end
+
+	def show
+	end
+
+	def new
+	  @software = Software.new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+
+	private
+
+	def software_params
+  	  params.require(:software).permit(:name, :url, :demo_url, :category)
+    end
+
+	def set_software
+	  @software = Software.find(params[:id])
+	end
+
+
 end
