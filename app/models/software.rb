dff78621f08d309afe28dfa13f563a72bc011394
vanessa massiot <vanessa.massiot@free.fr>
2020-05-28 15:19:05 +0200

    include in model : software / name & category , feature / name

--- app/models/software.rb
+++ app/models/software.rb
@@ -1,4 +1,7 @@
 class Software < ApplicationRecord
+	include PgSearch::Model
+    multisearchable against: [:name, :category]
+
 	has_many :software_plans
 	validates :name, presence: :true, uniqueness: :true
 	validates :category, presence: :true
