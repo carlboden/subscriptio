2c414573cad1d0e794a45af621134dbff2deb924
vanessa massiot <vanessa.massiot@free.fr>
2020-05-28 13:07:16 +0200

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,10 @@
 require 'faker'
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroy user, company, subscriptioplan, ratings"
+Rating.destroy_all
 User.destroy_all
 Company.destroy_all
 SubscriptioPlan.destroy_all
+
 
 puts "Destroy softwares Feature, Feature, Softwareplan, Software...."
 SoftwareFeature.destroy_all
