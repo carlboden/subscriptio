75fd52000fadfcdf4afc7a12f9999e827057471f
thibault mambour <thibault.mambour@protonmail.com>
2020-06-04 16:55:12 +0200

    Refactor alternatives + better index

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,9 +5,9 @@ class SubscriptionsController < ApplicationController
         @subscriptions = Subscription.where(:company_id => params[:company_id])
 
         @lowest_price_same_range_number_user = calculate_cheaper_plan_range_user(@subscriptions)
-         
+
+        @all_alternative_hash = update_calculation(@subscriptions)
         
-        @all_alternative_hash = calculate_alternative_price(@subscriptions)
         if params[:query2].present?
           @subscriptions = Subscription.where(:software_plan_id => SoftwarePlan.where(software_id: Software.where("name ILIKE ?", "%#{params[:query2]}%")), :company_id => params[:company_id])
           array_subscription_in_scope = []
@@ -117,173 +117,132 @@ class SubscriptionsController < ApplicationController
   end
 
 
+  def update_calculation(subscriptions)
+    all_alternative_hash = {}
 
-    def calculate_alternative_price(subscriptions)
+    subscriptions.each do |sub|
+      alt_soft = Software.includes(:subscriptions, software_plans: :features).where( category: sub.software.category)
+      alt_plans = (alt_soft).map(&:software_plans).flatten.select do |soft|
+         (sub.software_plan.features | soft.features).size > sub.software_plan.features.size / 2 
+      end 
+    
+      compares_subscriptions = alt_plans.map(&:subscriptions).flatten
 
-      all_alternative_hash = {}
-      hash_features_all_subscriptions = {}
-      subscriptions.each do |subscription|
-        features = subscription.software_plan.software_features
-        hash_features_all_subscriptions[subscription.id] = []
-        features.each {|feature| hash_features_all_subscriptions[subscription.id] << feature.feature_id}
+      lowest_price_same_features = calculate_cheaper_plan_range_user(compares_subscriptions)
+      lowest_price = {sub.id => [0, 15000000]}
+      lowest_price_same_features.each do |key, subscription_same_feature|
+        if lowest_price.values[0][1] > subscription_same_feature[0].price
+          lowest_price = {sub.id =>[key, subscription_same_feature[0].price] }
+        end
       end
 
-
-      hash_features_all_subscriptions.each do |subscription_id, array_feature|
-        sub = Subscription.find(subscription_id)
-        alternatives_software_same_category = Software.where(:category => sub.software_plan.software.category)
-        alternative_software_plan_same_category = []
-
-        alternatives_software_same_category.each do |alternative|
-          plans = SoftwarePlan.where(:software_id => alternative.id)
-          plans.each {|plan| alternative_software_plan_same_category << plan}
-        end
-        features_all_software_plan_hash = {}
-        alternative_software_plan_same_category.each do |plan|
-          all_features_plan = plan.software_features
-          features_all_software_plan_hash[all_features_plan[0].software_plan_id] = []
-          all_features_plan.each {|feature| features_all_software_plan_hash[all_features_plan[0].software_plan_id] << feature.feature_id}
-        end
-
-        array_same_feature_software_plan = []
-
-        features_all_software_plan_hash.each do |key, features|
-          right = {true: 0, false: 0}
-          array_feature[1..10].each do |feature|
-            if features.include? feature
-              right[:true] += 1
-            else
-              right[:false] += 1
-            end
-
-          end
-
-          right[:false] < 5 ? array_same_feature_software_plan << key : ""
-
-        end
-
-
-
-        subscriptions_same_features = Subscription.where(:software_plan_id =>  array_same_feature_software_plan)
-
-        lowest_price_same_features = calculate_cheaper_plan_range_user(subscriptions_same_features)
-        lowest_price = {subscription_id => [0, 1500]}
-        lowest_price_same_features.each do |key, subscription|
-          if lowest_price.values[0][1] > subscription[0].price
-            lowest_price = {subscription_id =>[key, subscription[0].price] }
-          end
-        end
-
-        all_alternative_hash = all_alternative_hash.merge(lowest_price)
-
-      end
-      all_alternative_hash
+      all_alternative_hash = all_alternative_hash.merge(lowest_price)
     end
+    all_alternative_hash
+  end
+
+  def calculate_cheaper_plan_range_user(subscriptions)
+    lowest_price_same_range_number_user = {}
+    red_light_price = {}
+    subscriptions.each do |subscription|
+      subscription_decreasing_order = Subscription.order('price ASC').where(:software_plan_id => subscription.software_plan_id)
+      subscription_in_range = {}
+      subscription_in_range[subscription.id] = []
+      range_user = (subscription.number_of_user - (1 + (subscription.number_of_user/2)**2))..(subscription.number_of_user + (1 + (subscription.number_of_user/2)**2))
+      subscription_decreasing_order.each do |sub|
+          if range_user === sub.number_of_user
+              subscription_in_range[subscription.id] << sub
+          end
+      end
+
+      subscription_in_range.each do |key, subscriptio|
+        if subscription_in_range[key][0].price < subscription.price
+          red_light_price[key] = [subscription_in_range[key][0], subscription]
+        else
+          lowest_price_same_range_number_user[key] = [subscription_in_range[key][0], subscription]
+        end
+      end
+    end
+    lowest_price_same_range_number_user = red_light_price.merge(lowest_price_same_range_number_user)
+    
+  end
 
 
+  def create_data_histogram
 
-    def calculate_cheaper_plan_range_user(subscriptions)
-      lowest_price_same_range_number_user = {}
-      red_light_price = {}
+    @expenses_per_month = {"2020-01" => 0, "2020-02" => 0, "2020-03" => 0, "2020-04" => 0, "2020-05" => 0, "2020-06" => 0, "2020-07" => 0, "2020-08" => 0, "2020-09" => 0, "2020-10" => 0, "2020-11" => 0, "2020-12" => 0}
+      @low_expenses_per_month = {"2020-01" => 0, "2020-02" => 0, "2020-03" => 0, "2020-04" => 0, "2020-05" => 0, "2020-06" => 0, "2020-07" => 0, "2020-08" => 0, "2020-09" => 0, "2020-10" => 0, "2020-11" => 0, "2020-12" => 0}
+      @sum_expenses = 0
+      @low_sum_expenses = 0
+      @subscription_status = {"You already have the best plan" => 0, "Cheaper Plan Available" => 0}
+      @subscription_range_price = {"0 - 25€" => 0, "26 - 50€" => 0, "51 - 100€" => 0, "101 - 250€" => 0, "251 - 500€" => 0, "501€ +" => 0}
+      @sum_expenses = 0
+      @low_sum_expenses = 0
+      subscriptions = Subscription.order(:start_date).where(:software_plan_id => @subscription.software_plan_id)
       subscriptions.each do |subscription|
-        subscription_decreasing_order = Subscription.order('price ASC').where(:software_plan_id => subscription.software_plan_id)
-        subscription_in_range = {}
-        subscription_in_range[subscription.id] = []
+        subscription_in_range = []
+        subscription_decreasing_order = Subscription.order('price ASC').where(:software_plan_id => @subscription.software_plan_id)
         range_user = (subscription.number_of_user - (1 + (subscription.number_of_user/2)**2))..(subscription.number_of_user + (1 + (subscription.number_of_user/2)**2))
-        subscription_decreasing_order.each do |sub|
-            if range_user === sub.number_of_user
-                subscription_in_range[subscription.id] << sub
+        subscription_decreasing_order.each do |subscription|
+            if range_user === subscription.number_of_user
+                subscription_in_range << subscription
             end
         end
 
-        subscription_in_range.each do |key, subscriptio|
-          if subscription_in_range[key][0].price < subscription.price
-            red_light_price[key] = [subscription_in_range[key][0], subscription]
-          else
-            lowest_price_same_range_number_user[key] = [subscription_in_range[key][0], subscription]
-          end
+        subscription.price == subscription_in_range[0].price ? @subscription_status["You already have the best plan"] += 1 : @subscription_status["Cheaper Plan Available"] += 1
+        if (0..25) === ( subscription.price  * subscription.number_of_user )
+        @subscription_range_price["0 - 25€"] += 1
+        elsif (26..50) === ( subscription.price  * subscription.number_of_user )
+        @subscription_range_price["26 - 50€"] += 1
+        elsif (51..100) === ( subscription.price  * subscription.number_of_user )
+        @subscription_range_price["51 - 100€"] += 1
+        elsif (101..250) === ( subscription.price  * subscription.number_of_user )
+        @subscription_range_price["101 - 250€"] += 1
+        elsif (251..500) === ( subscription.price  * subscription.number_of_user )
+        @subscription_range_price["251 - 500€"] += 1
+        else
+        @subscription_range_price["501€ +"] += 1
         end
-      end
-      lowest_price_same_range_number_user = red_light_price.merge(lowest_price_same_range_number_user)
-      
+
+
+        start_date = strip_date(subscription.start_date)
+        end_date = strip_date(subscription.end_date)
+
+        # To have if the month is before 10, keep the format 2020-05
+        udpate_format_date = start_date[0..3] + '-' + ( start_date[4..7].to_i < 10 ?  "0" + start_date[4..7].to_i.to_s : start_date[4..7])
+        if @expenses_per_month[udpate_format_date] == nil
+
+            @expenses_per_month[udpate_format_date] = ( subscription.price * subscription.number_of_user )
+            @low_expenses_per_month[udpate_format_date] = ( subscription_in_range[0].price * subscription.number_of_user )
+            @sum_expenses += ( subscription.price * subscription.number_of_user )
+            @low_sum_expenses += ( subscription_in_range[0].price * subscription.number_of_user )
+        else
+            @expenses_per_month[udpate_format_date] += ( subscription.price * subscription.number_of_user )
+            @low_expenses_per_month[udpate_format_date] += ( subscription_in_range[0].price * subscription.number_of_user )
+            @sum_expenses += ( subscription.price * subscription.number_of_user )
+            @low_sum_expenses += ( subscription_in_range[0].price * subscription.number_of_user )
+        end
+
+        start_date = add_one_day_strip_date(start_date)
+
+        while start_date.to_i <= end_date.to_i
+            udpate_format_date = start_date[0..3] + '-' + ( start_date[4..7].to_i < 10 ? "0" + start_date[4..7] : start_date[4..7] )
+            if @expenses_per_month[udpate_format_date] == nil
+                @expenses_per_month[udpate_format_date] = ( subscription.price * subscription.number_of_user )
+                @low_expenses_per_month[udpate_format_date] = ( subscription_in_range[0].price * subscription.number_of_user )
+                @sum_expenses += ( subscription.price * subscription.number_of_user )
+                @low_sum_expenses += ( subscription_in_range[0].price * subscription.number_of_user )
+            else
+                @expenses_per_month[udpate_format_date] += ( subscription.price * subscription.number_of_user )
+                @low_expenses_per_month[udpate_format_date] += ( subscription_in_range[0].price * subscription.number_of_user )
+                @sum_expenses += ( subscription.price * subscription.number_of_user )
+                @low_sum_expenses += ( subscription_in_range[0].price * subscription.number_of_user )
+            end
+            start_date = add_one_day_strip_date(start_date)
+
+        end
     end
-
-
-    def create_data_histogram
-
-      @expenses_per_month = {"2020-01" => 0, "2020-02" => 0, "2020-03" => 0, "2020-04" => 0, "2020-05" => 0, "2020-06" => 0, "2020-07" => 0, "2020-08" => 0, "2020-09" => 0, "2020-10" => 0, "2020-11" => 0, "2020-12" => 0}
-        @low_expenses_per_month = {"2020-01" => 0, "2020-02" => 0, "2020-03" => 0, "2020-04" => 0, "2020-05" => 0, "2020-06" => 0, "2020-07" => 0, "2020-08" => 0, "2020-09" => 0, "2020-10" => 0, "2020-11" => 0, "2020-12" => 0}
-        @sum_expenses = 0
-        @low_sum_expenses = 0
-        @subscription_status = {"You already have the best plan" => 0, "Cheaper Plan Available" => 0}
-        @subscription_range_price = {"0 - 25€" => 0, "26 - 50€" => 0, "51 - 100€" => 0, "101 - 250€" => 0, "251 - 500€" => 0, "501€ +" => 0}
-        @sum_expenses = 0
-        @low_sum_expenses = 0
-        subscriptions = Subscription.order(:start_date).where(:software_plan_id => @subscription.software_plan_id)
-        subscriptions.each do |subscription|
-          subscription_in_range = []
-          subscription_decreasing_order = Subscription.order('price ASC').where(:software_plan_id => @subscription.software_plan_id)
-         range_user = (subscription.number_of_user - (1 + (subscription.number_of_user/2)**2))..(subscription.number_of_user + (1 + (subscription.number_of_user/2)**2))
-         subscription_decreasing_order.each do |subscription|
-             if range_user === subscription.number_of_user
-                 subscription_in_range << subscription
-             end
-         end
-
-         subscription.price == subscription_in_range[0].price ? @subscription_status["You already have the best plan"] += 1 : @subscription_status["Cheaper Plan Available"] += 1
-         if (0..25) === ( subscription.price  * subscription.number_of_user )
-          @subscription_range_price["0 - 25€"] += 1
-         elsif (26..50) === ( subscription.price  * subscription.number_of_user )
-          @subscription_range_price["26 - 50€"] += 1
-         elsif (51..100) === ( subscription.price  * subscription.number_of_user )
-          @subscription_range_price["51 - 100€"] += 1
-         elsif (101..250) === ( subscription.price  * subscription.number_of_user )
-          @subscription_range_price["101 - 250€"] += 1
-         elsif (251..500) === ( subscription.price  * subscription.number_of_user )
-          @subscription_range_price["251 - 500€"] += 1
-         else
-          @subscription_range_price["501€ +"] += 1
-         end
-
-
-          start_date = strip_date(subscription.start_date)
-          end_date = strip_date(subscription.end_date)
-
-          # To have if the month is before 10, keep the format 2020-05
-          udpate_format_date = start_date[0..3] + '-' + ( start_date[4..7].to_i < 10 ?  "0" + start_date[4..7].to_i.to_s : start_date[4..7])
-          if @expenses_per_month[udpate_format_date] == nil
-
-              @expenses_per_month[udpate_format_date] = ( subscription.price * subscription.number_of_user )
-              @low_expenses_per_month[udpate_format_date] = ( subscription_in_range[0].price * subscription.number_of_user )
-              @sum_expenses += ( subscription.price * subscription.number_of_user )
-              @low_sum_expenses += ( subscription_in_range[0].price * subscription.number_of_user )
-          else
-              @expenses_per_month[udpate_format_date] += ( subscription.price * subscription.number_of_user )
-              @low_expenses_per_month[udpate_format_date] += ( subscription_in_range[0].price * subscription.number_of_user )
-              @sum_expenses += ( subscription.price * subscription.number_of_user )
-              @low_sum_expenses += ( subscription_in_range[0].price * subscription.number_of_user )
-          end
-
-          start_date = add_one_day_strip_date(start_date)
-
-          while start_date.to_i <= end_date.to_i
-              udpate_format_date = start_date[0..3] + '-' + ( start_date[4..7].to_i < 10 ? "0" + start_date[4..7] : start_date[4..7] )
-              if @expenses_per_month[udpate_format_date] == nil
-                  @expenses_per_month[udpate_format_date] = ( subscription.price * subscription.number_of_user )
-                  @low_expenses_per_month[udpate_format_date] = ( subscription_in_range[0].price * subscription.number_of_user )
-                  @sum_expenses += ( subscription.price * subscription.number_of_user )
-                  @low_sum_expenses += ( subscription_in_range[0].price * subscription.number_of_user )
-              else
-                  @expenses_per_month[udpate_format_date] += ( subscription.price * subscription.number_of_user )
-                  @low_expenses_per_month[udpate_format_date] += ( subscription_in_range[0].price * subscription.number_of_user )
-                  @sum_expenses += ( subscription.price * subscription.number_of_user )
-                  @low_sum_expenses += ( subscription_in_range[0].price * subscription.number_of_user )
-              end
-              start_date = add_one_day_strip_date(start_date)
-
-          end
-      end
-    end
+  end
 
 
     def params_subscription
--- app/models/software.rb
+++ app/models/software.rb
@@ -7,7 +7,10 @@ class Software < ApplicationRecord
                     tsearch: { prefix: true }
                   }
 
-	has_many :software_plans
+  has_many :software_plans
+  has_many :software_features, through: :software_plans
+  has_many :features, through: :software_features
+  has_many :subscriptions, through: :software_plans
   has_one_attached :photo
 	validates :name, presence: :true, uniqueness: :true
 	validates :category, presence: :true
