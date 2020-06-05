20707d2b3865f245e604fdea17c3f72ae1e2c89d
thibault mambour <thibault.mambour@protonmail.com>
2020-06-05 10:02:17 +0200

    step 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,8 @@ class ApplicationController < ActionController::Base
       user_path(current_user.id)
     end
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
